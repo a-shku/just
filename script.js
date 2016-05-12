11f3be0935fb360963e64fef3c66d36f99c6c670
energy1 <dobro@topdetal.com.ua>
2016-05-12 22:10:41 +0300

    Add files via upload
    на отправку

--- script.js
+++ script.js
@@ -7,6 +7,43 @@ ymaps.ready(function () {
         }, {
             searchControlProvider: 'yandex#search'
         }),
+		/*тест макета*/
+			 BalloonContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="margin: 10px;">' +
+                '<b>{{properties.name}}</b><br />' +
+                '<i id="count"></i> ' +
+                '<button id="counter-button"> +1 </button>' +
+            '</div>', {
+
+            // Переопределяем функцию build, чтобы при создании макета начинать
+            // слушать событие click на кнопке-счетчике.
+            build: function () {
+                // Сначала вызываем метод build родительского класса.
+                BalloonContentLayout.superclass.build.call(this);
+                // А затем выполняем дополнительные действия.
+                $('#counter-button').bind('click', this.onCounterClick);
+                $('#count').html(counter);
+            },
+
+            // Аналогично переопределяем функцию clear, чтобы снять
+            // прослушивание клика при удалении макета с карты.
+            clear: function () {
+                // Выполняем действия в обратном порядке - сначала снимаем слушателя,
+                // а потом вызываем метод clear родительского класса.
+                $('#counter-button').unbind('click', this.onCounterClick);
+                BalloonContentLayout.superclass.clear.call(this);
+            },
+
+            onCounterClick: function () {
+                $('#count').html(++counter);
+                if (counter == 5) {
+                    alert('Вы славно потрудились.');
+                    counter = 0;
+                    $('#count').html(counter);
+                }
+            }
+        });
+		/*тест макета*/
              // Создаем собственный макет с информацией о выбранном геообъекте.
 		customItemContentLayout = ymaps.templateLayoutFactory.createClass(
         // Флаг "raw" означает, что данные вставляют "как есть" без экранирования html.
@@ -39,7 +76,7 @@ ymaps.ready(function () {
             };
         },
        points = [
-            [49.951903,36.211961], [49.953972,36.259610], [49.996188294179, 36.110912435790915], [49.981329,36.242781], [49.954708,36.248870], [49.953123,36.206067], [49.998585,36.254980]
+            [49.951903,36.211961], [49.996188294179, 36.101912435790915], [49.981329,36.242781], [49.954708,36.248870], [49.953123,36.206067]
         ],
         geoObjects = [];
     for(var i = 0, len = points.length; i < len; i++) {
@@ -81,55 +118,50 @@ ymaps.ready(function () {
 			}).then(function(present){
 			/*окно добавления*/
 			console.log(present);
+			var wrapForRev;
 			if(present.length < 1){
-				present = 'нет отзывов'
+				wrapForRev = 'нет отзывов'
 			} else{
 				var list = document.createElement('ul');
-				
+					list.classList.add('reviewsList');
 				for(var i = 0; i<present.length; i++){
-					//console.log(list);
+						delete present[i].coords;
 					var li = document.createElement('li');
 					for(var getReviews in present[i]){
-						console.log(getReviews +' = ' + present[i][getReviews]);
+						
+						if(getReviews == 'date'){
+							//alert(new Date(present[i][getReviews]));
+							var date = new Date(present[i][getReviews]);
+								console.log(getReviews +' = ' + date);
+					li.innerHTML += '<p class="date">'+date+'</p>';
+						}
+						else if(getReviews == 'address'){}
+						else{
+							console.log(getReviews +' = ' + present[i][getReviews]);
 					li.innerHTML += '<p>'+present[i][getReviews]+'</p>';
+						}
+						
 					}
 					list.appendChild(li);
 				}
-				//document.getElementById('pres').appendChild(list);
-				console.log(list);
-				//console.log(document.getElementById('pres'));
-				
+								
 				var wrapForRev = document.createElement('div');
-					console.log(wrapForRev);
 					wrapForRev.setAttribute('id', 'pres');
-					//wrapForRev.setAttribute('class', 'class2');
-					//console.log(wrapForRev);
-					//console.log(document.getElementsByClassName('class2'));
-				//var elemForRev = document.getElementById('pres');
-				//var placeForReview = document.getElementsByClassName('class2');
 				
-				wrapForRev.innerHTML=list;
-				console.log(wrapForRev.innerHTML);
-				
-				//console.log(wrapForRev);
+				wrapForRev.appendChild(list);
+				var wrapForRev = wrapForRev.innerHTML;
 			}
 			
 			new Promise(function(resolve, reject){
 				console.log(wrapForRev);
 				if (!myMap.balloon.isOpen()) {
-            //console.log(present);
-			
-            myMap.balloon.open(coords, {
+					myMap.balloon.open(coords,
 				
-                contentHeader: gotAddress.properties.get('text')/*+gotAddress.properties.get('name')*/,
-                contentBody: wrapForRev+' <p>Кто-то щелкнул по карте.</p>' +present+
-				    '<p>Координаты щелчка: ' + [
-                    coords[0].toPrecision(6),
-                    coords[1].toPrecision(6)
-                    ].join(', ') + '</p>'+'<input id="name1" placeholder="Name"><br><input id="place" placeholder="Place"><br>'+'<textarea id="review" placeholder="review"></textarea>',
+				{contentHeader: gotAddress.properties.get('text')/*+gotAddress.properties.get('name')*/,
+                contentBody: '<div style="max-height:150px; overflow-y:auto;">'+wrapForRev+'</div>' +
+				    '<p>ОСТАВИТЬ ОТЗЫВ</p>'+'<input id="name1" placeholder="Name"><br><input id="place" placeholder="Place"><br>'+'<textarea id="review" placeholder="review"></textarea>',
                 contentFooter:'<button id="send" class="class1">Send</button>'
             });
-				//console.log(document.getElementById('send'));
 			}
 			else {
 				myMap.balloon.close();
